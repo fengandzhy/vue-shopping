8a12f1ad43f6423db42d2d3be0ce9710c15aecb9
fengandzhy <fengandzhy@gmail.com>
2024-04-08 22:53:55 +1200

    shopping-site add a logo picture

--- shopping-site/src/components/header/headerIndex.vue
+++ shopping-site/src/components/header/headerIndex.vue
@@ -277,108 +277,108 @@
         }
       }
 
-      .lifeservices {
-        border-right: 1px solid #e4e4e4;
-        overflow: hidden;
-        display: flex;
-        flex-wrap: wrap;
-
-        .life-item {
-          border-left: 1px solid #e4e4e4;
-          border-bottom: 1px solid #e4e4e4;
-          margin-right: -1px;
-          height: 64px;
-          text-align: center;
-          position: relative;
-          cursor: pointer;
-          width: 25%;
-
-          .list-item {
-            background-image: url(../images/home/icons.png);
-            width: 61px;
-            height: 40px;
-            display: block;
-          }
-
-          .service-intro {
-            line-height: 22px;
-            width: 60px;
-            display: block;
-          }
-
-          &:nth-child(1) {
-            .list-item {
-              background-position: 0px -5px;
-            }
-          }
-
-          &:nth-child(2) {
-            .list-item {
-              background-position: -62px -5px;
-            }
-          }
-
-          &:nth-child(3) {
-            .list-item {
-              background-position: -126px -5px;
-            }
-          }
-
-          &:nth-child(4) {
-            .list-item {
-              background-position: -190px -5px;
-            }
-          }
-
-          &:nth-child(5) {
-            .list-item {
-              background-position: 0px -76px;
-            }
-          }
-
-          &:nth-child(6) {
-            .list-item {
-              background-position: -62px -76px;
-            }
-          }
-
-          &:nth-child(7) {
-            .list-item {
-              background-position: -126px -76px;
-            }
-          }
-
-          &:nth-child(8) {
-            .list-item {
-              background-position: -190px -76px;
-            }
-          }
-
-          &:nth-child(9) {
-            .list-item {
-              background-position: 0px -146px;
-            }
-          }
-
-          &:nth-child(10) {
-            .list-item {
-              background-position: -62px -146px;
-            }
-          }
-
-          &:nth-child(11) {
-            .list-item {
-              background-position: -126px -146px;
-            }
-          }
-
-          &:nth-child(12) {
-            .list-item {
-              background-position: -190px -146px;
-            }
-          }
-        }
-      }
+      //.lifeservices {
+      //  border-right: 1px solid #e4e4e4;
+      //  overflow: hidden;
+      //  display: flex;
+      //  flex-wrap: wrap;
+      //
+      //  .life-item {
+      //    border-left: 1px solid #e4e4e4;
+      //    border-bottom: 1px solid #e4e4e4;
+      //    margin-right: -1px;
+      //    height: 64px;
+      //    text-align: center;
+      //    position: relative;
+      //    cursor: pointer;
+      //    width: 25%;
+      //
+      //    .list-item {
+      //      background-image: url(../images/home/icons.png);
+      //      width: 61px;
+      //      height: 40px;
+      //      display: block;
+      //    }
+      //
+      //    .service-intro {
+      //      line-height: 22px;
+      //      width: 60px;
+      //      display: block;
+      //    }
+      //
+      //    &:nth-child(1) {
+      //      .list-item {
+      //        background-position: 0px -5px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(2) {
+      //      .list-item {
+      //        background-position: -62px -5px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(3) {
+      //      .list-item {
+      //        background-position: -126px -5px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(4) {
+      //      .list-item {
+      //        background-position: -190px -5px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(5) {
+      //      .list-item {
+      //        background-position: 0px -76px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(6) {
+      //      .list-item {
+      //        background-position: -62px -76px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(7) {
+      //      .list-item {
+      //        background-position: -126px -76px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(8) {
+      //      .list-item {
+      //        background-position: -190px -76px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(9) {
+      //      .list-item {
+      //        background-position: 0px -146px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(10) {
+      //      .list-item {
+      //        background-position: -62px -146px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(11) {
+      //      .list-item {
+      //        background-position: -126px -146px;
+      //      }
+      //    }
+      //
+      //    &:nth-child(12) {
+      //      .list-item {
+      //        background-position: -190px -146px;
+      //      }
+      //    }
+      //  }
+      //}
 
       .ads {
         margin-top: 5px;
@@ -467,15 +467,15 @@
           margin: 5px 0;
         }
 
-        .img {
-          i {
-            width: 35px;
-            height: 35px;
-            display: block;
-            background: url(../images/home/bg0.png);
-            margin-left: 10px;
-          }
-        }
+        //.img {
+        //  i {
+        //    width: 35px;
+        //    height: 35px;
+        //    display: block;
+        //    background: url(../images/home/bg0.png);
+        //    margin-left: 10px;
+        //  }
+        //}
 
         .text {
           line-height: 28px;
@@ -575,18 +575,18 @@
         line-height: 30px;
       }
 
-      .fr {
-        float: right;
-        background-image: url(../images/home/icons.png);
-        width: 66px;
-        height: 25px;
-        background-position: 182px -104px;
-        line-height: 30px;
-        font-size: 12px;
-        font-weight: 400;
-        color: #666;
-        text-decoration: none;
-      }
+      //.fr {
+      //  float: right;
+      //  background-image: url(../images/home/icons.png);
+      //  width: 66px;
+      //  height: 25px;
+      //  background-position: 182px -104px;
+      //  line-height: 30px;
+      //  font-size: 12px;
+      //  font-weight: 400;
+      //  color: #666;
+      //  text-decoration: none;
+      //}
     }
 
     .bd {
